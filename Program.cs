e15e9e0c42dc63dc3b392f6261f3902c4cd5c82f
BinhTran <fam.out16@gmail.com>
2024-08-05 21:44:15 +0700

    Add cac files cua Bai thuc hanh Su dung toan tu Tinh dien tich hinh chu nhat

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SUDUNGTOANTU
+{
+    class TinhDienTich
+    {
+        static void Main(string[] args)
+        {
+            float chieucao;
+            float chieurong;
+            float dientich;
+            Console.WriteLine("Nhap chieu cao hinh chu nhat:");
+            chieucao = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap chieu rong hinh chu nhat:");
+            chieurong = float.Parse(Console.ReadLine());
+            dientich = chieucao * chieurong;
+            Console.WriteLine("Dien tich hinh chu nhat: {0} * {1} = {2}", chieucao, chieurong, dientich);
+        }
+    }
+}
